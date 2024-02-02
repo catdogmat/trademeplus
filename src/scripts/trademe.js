6d57b330f2864458c660b1ea267700525f4e8d29
catdogmat <45089908+catdogmat@users.noreply.github.com>
2024-02-02 20:48:42 +1300

    Finally add cors support!

--- src/scripts/trademe.js
+++ src/scripts/trademe.js
@@ -1,10 +1,27 @@
-console.log(
-" _______            _        __  __              " +
-"|__   __|          | |      |  \\/  |         _   " +
-"   | |_ __ __ _  __| | ___  | \\  / | ___   _| |_ " +
-"   | | '__/ _` |/ _` |/ _ \ | |\\/| |/ _ \ |_   _|" +
-"   | | | | (_| | (_| |  __/ | |  | |  __/   |_|  " +
-"   |_|_|  \\__,_|\\__,_|\\___| |_|  |_|\\___|        " +
-"                                                 " +
-"            W e ' r e   s e c o n d .            "
-)
+setTimeout(() => {
+    console.log(
+        "%c _______            _        __  __              \n" +
+        "|__   __|          | |      |  \\/  |         _   \n" +
+        "   | |_ __ __ _  __| | ___  | \\  / | ___   _| |_ \n" +
+        "   | | '__/ _` |/ _` |/ _ \\ | |\\/| |/ _ \\ |_   _|\n" +
+        "   | | | | (_| | (_| |  __/ | |  | |  __/   |_|  \n" +
+        "   |_|_|  \\__,_|\\__,_|\\___| |_|  |_|\\___|        \n" +
+        "                                                 \n" +
+        "            W e ' r e   s e c o n d .            ",
+        "font-family: monospace; font-weight: bold;"
+    )
+}, 900);
+
+    // Example: Fetching data from the same domain (trademe.co.nz)
+fetch('https://www.trademe.co.nz/')
+.then(response => response.text())
+.then(data => {
+    // Process the data
+    console.log("-----------------------------------DATA------------------------------");
+    console.log(data);
+})
+.catch(error => {
+    console.error(error);
+});
+
+  
